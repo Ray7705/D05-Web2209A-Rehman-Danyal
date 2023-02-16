1b74c899fe12394ff3751a29ff1865383c2dca81
DanialBeh <120136265+DanialBeh@users.noreply.github.com>
2023-02-15 19:33:15 -0500

    store structure

---
+++ ShoppingList/Models/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Models
+{
+    internal class Cart
+    {
+        public int Quantity { get; set; }
+        
+        // list of products
+
+
+
+    }
+}
---
+++ ShoppingList/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Models
+{
+    internal class Product
+    {
+        public string? Name { get;private set; }
+        public int Id { get; }
+        public decimal Price { get;private set; }
+
+
+
+    }
+}
